1c6c04d4d4670360bb20ad35545fcdf9e76b6226
ArianV <arianvahdat03@gmail.com>
2025-09-16 12:49:52 -0700

    added header

--- views/layout.php
+++ views/layout.php
@@ -10,6 +10,20 @@
   <?= $head ?? '' ?>
 </head>
 <body class="<?= e($bodyCls ?? '') ?>">
+  
+  <header class="site-header">
+    <a class="brand" href="<?= BASE_URL ?>">Music Landing</a>
+    <nav>
+      <?php if ($u): ?>
+        <a href="<?= BASE_URL ?>dashboard">Dashboard</a>
+        <a href="<?= BASE_URL ?>logout">Logout</a>
+      <?php else: ?>
+        <a href="<?= BASE_URL ?>login">Login</a>
+        <a href="<?= BASE_URL ?>register">Register</a>
+      <?php endif; ?>
+    </nav>
+  </header>
+  
   <?= $content ?? '' ?>
 </body>
 </html>
